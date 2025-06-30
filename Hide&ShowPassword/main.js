3a8644dc1959e0433b71d7ac1a1eb479f7468edc
llyas36 <eliyasinbox1@gmail.com>
2025-06-30 15:36:29 +0300

    hide & show password

--- Hide&ShowPassword/main.js
+++ Hide&ShowPassword/main.js
@@ -1,6 +1,7 @@
 togglePasswordBtn = document.getElementById("togglePassword");
 passwordInput = document.getElementById("passwordInput");
 
+//handling the toggle icon
 togglePasswordBtn.addEventListener("click", () => {
   if (passwordInput.type == "password") {
     passwordInput.type = "text";
